375c3792b42ba615c2da432003b0b9b615029564
bafirgenius <bafirgenius@gmail.com>
2021-07-13 15:04:53 +0700

    first commit

--- src/models/users.js
+++ src/models/users.js
@@ -2,9 +2,8 @@
 const mongoose = require('mongoose')
 
 const userSchema = mongoose.Schema({
-    nama: { type: String },
-    hobi: { type: String },
-    alamat: { type: String },
-    nomer_telp: { type: Number }
+    name: { type: String },
+    email: { type: String },
+    age: { type: Number }
   });
   module.exports = mongoose.model('users', userSchema);
